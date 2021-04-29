beec9795561cf661065eb9f80726ad53ed74108c
AJAY <ajaynairarun@gmail.com>
2021-04-29 22:01:20 +0530

    started making records page

---
+++ src/Components/Records.js
@@ -0,0 +1,33 @@
+import React,{Component} from 'react'
+
+class Records extends Component{
+    constructor(){
+        super()
+        this.state={}
+    }
+
+    render(){
+        return(
+            <div id='Records'>
+                <div id='navbar'>
+
+                </div>
+                <div id='controls_and_records'>
+                    <div id='controls'>
+
+                    </div>
+
+
+
+                    <div id='chart'>
+
+                    </div>
+
+                </div>
+
+            </div>
+        )
+    }
+}
+
+export default Records
